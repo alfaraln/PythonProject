237fb671cc8a602d98d6dd0505a9b6b049f2509a
Lamah Nasrallah Alfaraj <alfaraln@gmail.com>
2020-10-17 21:20:16 +0300

    Adding strs to enhance the look and feel

--- bike_share.py
+++ bike_share.py
@@ -90,14 +90,14 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
-    print("The most common month is ", df['month'].mode()[0], "\n")
+    print("****The most common month is **** ", df['month'].mode()[0], "\n")
 
     # display the most common day of week
-    print("The most common day of week is ", df['day_of_week'].mode()[0], "\n")
+    print("****The most common day of week is **** ", df['day_of_week'].mode()[0], "\n")
 
     # display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
-    print("The most common start hour is ", df['hour'].mode()[0])
+    print("****The most common start hour is **** ", df['hour'].mode()[0])
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
